a67903537f4e9462950ba6097164dee24b2977f9
JulianZea <cesarjulianzeabravo@gmail.com>
2023-01-27 13:21:04 -0500

    tema shorthand property names

---
+++ JS-for-REACT/shorthand_property_names/shorthand_property_name.js
@@ -0,0 +1,22 @@
+const name = "laptop"
+const price = 3000
+
+const newProduct = {
+    name: name,
+    price: price
+}
+
+console.log(newProduct)
+
+/* otra manera, el resultado es el mismo*/
+/*
+const name = "laptop"
+const price = 3000
+
+const newProduct = {
+    name,
+    price
+}
+
+console.log(newProduct)
+*/
